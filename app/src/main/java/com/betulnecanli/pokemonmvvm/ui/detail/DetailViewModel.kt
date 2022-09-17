b779d3dea14bf82fcd41b7befd0c520e2a10efbe
betulnecanli <betulnecanli@gmail.com>
2022-09-17 18:45:15 +0300

    Dependency injection setup

--- app/src/main/java/com/betulnecanli/pokemonmvvm/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/betulnecanli/pokemonmvvm/ui/detail/DetailViewModel.kt
@@ -1,9 +1,13 @@
 package com.betulnecanli.pokemonmvvm.ui.detail
 
+import androidx.lifecycle.ViewModel
+import com.betulnecanli.pokemonmvvm.repository.PokedexRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 
 @HiltViewModel
-class DetailViewModel @Inject constructor() {
+class DetailViewModel @Inject constructor(
+    val repository : PokedexRepository
+) : ViewModel() {
 }
--- app/src/main/java/com/betulnecanli/pokemonmvvm/ui/list/ListViewModel.kt
+++ app/src/main/java/com/betulnecanli/pokemonmvvm/ui/list/ListViewModel.kt
@@ -1,4 +1,11 @@
 package com.betulnecanli.pokemonmvvm.ui.list
 
-class ListViewModel {
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ListViewModel @Inject constructor(
+
+): ViewModel() {
 }
