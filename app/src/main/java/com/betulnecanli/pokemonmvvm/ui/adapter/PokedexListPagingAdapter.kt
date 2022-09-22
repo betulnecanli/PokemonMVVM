9943533e2432bc6ca19f219b52a63a79049f3ed0
betulnecanli <betulnecanli@gmail.com>
2022-09-22 18:31:50 +0300

    Dominant Color Settings

--- app/src/main/java/com/betulnecanli/pokemonmvvm/ui/adapter/PokedexListPagingAdapter.kt
+++ app/src/main/java/com/betulnecanli/pokemonmvvm/ui/adapter/PokedexListPagingAdapter.kt
@@ -1,14 +1,26 @@
 package com.betulnecanli.pokemonmvvm.ui.adapter
 
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.paging.PagingDataAdapter
+import androidx.palette.graphics.Palette
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import com.betulnecanli.pokemonmvvm.R
 import com.betulnecanli.pokemonmvvm.data.remote.responses.Result
 import com.betulnecanli.pokemonmvvm.databinding.PokeItemBinding
 import com.betulnecanli.pokemonmvvm.utils.getImageOfPoke
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 
 class PokedexListPagingAdapter : PagingDataAdapter<Result, PokedexListPagingAdapter.PokedexViewHolder>(diffCallback = diffCallBack)  {
 
@@ -26,14 +38,60 @@ class PokedexListPagingAdapter : PagingDataAdapter<Result, PokedexListPagingAdap
     }
     inner class PokedexViewHolder(val binding : PokeItemBinding) : RecyclerView.ViewHolder(binding.root){
 
+        var dominantColor: Int = 0
+
         fun bind(result: Result){
             binding.apply {
                 characterName.text=result.name
                 val imgLink = result.url.getImageOfPoke()
-                characterImg.load(imgLink){
-                    crossfade(true)
-                    crossfade(1000)
-                }
+
+                /* characterImg.load(imgLink){
+                     crossfade(true)
+                     crossfade(1000)
+                 }*/
+
+                Glide.with(root)
+                    .load(imgLink)
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .listener(object : RequestListener<Drawable>{
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            progressCircular.isVisible = false
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                           val drawable = resource as BitmapDrawable
+                            val bitmap = drawable.bitmap
+                            Palette.Builder(bitmap).generate{
+                                it?.let { palette ->
+                                    dominantColor = palette.getDominantColor(
+                                        ContextCompat.getColor(
+                                            root.context,
+                                            R.color.white
+                                        )
+                                    )
+                                    characterImg.setBackgroundColor(dominantColor)
+                                }
+                            }
+                            progressCircular.isVisible = false
+                            return false
+                        }
+
+                    })
+                    .into(characterImg)
+
+
             }
         }
 
