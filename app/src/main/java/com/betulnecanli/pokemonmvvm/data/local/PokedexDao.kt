fbe693c5002ba050d72fcec06759969fe5b47381
betulnecanli <betulnecanli@gmail.com>
2023-03-02 22:56:47 +0300

    Pokemon dao done

--- app/src/main/java/com/betulnecanli/pokemonmvvm/data/local/PokedexDao.kt
+++ app/src/main/java/com/betulnecanli/pokemonmvvm/data/local/PokedexDao.kt
@@ -1,9 +1,25 @@
 package com.betulnecanli.pokemonmvvm.data.local
 
+import androidx.paging.PagingSource
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.betulnecanli.pokemonmvvm.data.remote.responses.Pokemon
 
 @Dao
 interface PokedexDao {
 
+    @Query("SELECT * FROM PokemonTable ORDER BY id ASC")
+    fun getAllCharacters(): PagingSource<Int, Pokemon>
+
+    @Query("SELECT * FROM PokemonTable WHERE id =:id")
+    fun getSelectedCharacter(id:Int): Pokemon
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addCharacter(characters : List<Pokemon>)
+
+    @Query("DELETE FROM PokemonTable")
+    suspend fun deleteAll()
 
 }
